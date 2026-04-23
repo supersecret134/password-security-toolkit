5f8df68eea62b778afc32533f74e0d16ed896cb9
Amrit <as5318287@gmail.com>
2026-04-23 12:37:34 +0530

    Update hash_utils.py

--- password-toolkit/modules/hash_utils.py
+++ password-toolkit/modules/hash_utils.py
@@ -1,14 +1,16 @@
 import hashlib
 
-def hash_word(word, algorithm):
-    """Hash a word using the specified algorithm"""
-    word = word.encode('utf-8')
-    
-    if algorithm == 'md5':
-        return hashlib.md5(word).hexdigest()
-    elif algorithm == 'sha1':
-        return hashlib.sha1(word).hexdigest()
-    elif algorithm == 'sha256':
-        return hashlib.sha256(word).hexdigest()
+def hash_word(word, algo):
+    word = word.strip()
+
+    if algo == "md5":
+        return hashlib.md5(word.encode()).hexdigest()
+
+    elif algo == "sha1":
+        return hashlib.sha1(word.encode()).hexdigest()
+
+    elif algo == "sha256":
+        return hashlib.sha256(word.encode()).hexdigest()
+
     else:
-        raise ValueError(f"Unsupported algorithm: {algorithm}")
+        return ""
